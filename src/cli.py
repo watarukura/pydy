ef47e3d190f2942bb6849e178f29f65a624b4d7f
渉 倉嶋 <what.r.j150@gmail.com>
2020-06-06 13:23:03 +0000

    init

---
+++ src/cli.py
@@ -0,0 +1,24 @@
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option("--pkey", help="get-item")
+@click.option("--skey", default=None, help="get-item")
+def get(pkey, skey):
+    click.echo(f"get {pkey}, {skey}")
+
+
+@click.command()
+@click.option("--pkey", help="put-item")
+@click.option("--skey", default=None, help="put-item")
+def put(pkey, skey):
+    click.echo(f"put {pkey}, {skey}")
+
+
+cli.add_command(get)
+cli.add_command(put)
