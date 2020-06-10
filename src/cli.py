16dbdc9203f1ae7ea74672a8ef67841ba5a60c6b
wataru-kurashima <wataru-kurashima@hands-lab.com>
2020-06-10 13:24:28 +0900

    feat(list): list_tables()を仮実装

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,7 @@
+import boto3
 import click
+
+client = boto3.client("dynamodb")
 
 
 @click.group()
@@ -7,10 +10,13 @@ def cli() -> None:
 
 
 @click.command()
+@click.option("--table", help="get-item")
 @click.option("--pkey", help="get-item")
 @click.option("--skey", default=None, help="get-item")
-def get(pkey: str, skey: str) -> None:
-    click.echo(f"get {pkey}, {skey}")
+def get(table: str, pkey: str, skey: str) -> None:
+    response = client.get_item(TableName=table, Key={"S": pkey})
+    print(response)
+    # click.echo(f"get {pkey}, {skey}")
 
 
 @click.command()
@@ -20,5 +26,21 @@ def put(pkey: str, skey: str) -> None:
     click.echo(f"put {pkey}, {skey}")
 
 
+@click.command()
+def list() -> None:
+    try:
+        response = client.list_tables()
+        if last_evaluated_table_name := response["LastEvaluatedTableName"]:
+            add_table_list = client.list_tables(
+                ExclusiveStartTableName=last_evaluated_table_name)
+            response["TableNames"].append(add_table_list["TableNames"])
+    except Exception as e:
+        click.echo(response["ResponseMetadata"])
+        raise e
+
+    click.echo(response["TableNames"])
+
+
 cli.add_command(get)
 cli.add_command(put)
+cli.add_command(list)
