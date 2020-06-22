93aadd00a5608de47e935d96fb4647425d0cec5c
渉 倉嶋 <what.r.j150@gmail.com>
2020-06-22 21:31:21 +0000

    fix(test): add unittest for attribute_definitions

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List
 
 import pytest
 
-from src.util import get_key_names
+from src.util import generate_key_clause, get_key_names
 
 
 @pytest.mark.parametrize(
@@ -25,3 +25,46 @@ def test_get_key_name(
     pkey_name, skey_name = get_key_names(key_schema)
     assert pkey_name == expect_pkey_name
     assert skey_name == expect_skey_name
+
+
+@pytest.mark.parametrize(
+    "attr_def, pkey, pkey_name, skey,skey_name, expect",
+    (
+        (
+            [
+                {"AttributeName": "Id", "AttributeType": "S"},
+                {"AttributeName": "ReplyDateTime", "AttributeType": "S"},
+                {"AttributeName": "PostedBy", "AttributeType": "S"},
+            ],
+            "1",
+            "Id",
+            None,
+            None,
+            {"Id": "1"},
+        ),
+        (
+            [
+                {"AttributeName": "Id", "AttributeType": "S"},
+                {"AttributeName": "ReplyDateTime", "AttributeType": "S"},
+                {"AttributeName": "PostedBy", "AttributeType": "S"},
+            ],
+            "1",
+            "Id",
+            "20200623062000",
+            "ReplyDateTime",
+            {"Id": "1", "ReplyDateTime": "20200623062000"},
+        ),
+    ),
+)
+def test_generate_key_clause(
+    attr_def: List[Dict],
+    pkey: str,
+    pkey_name: str,
+    skey: str,
+    skey_name: str,
+    expect: str,
+):
+    key_clause = generate_key_clause(
+        attr_def, pkey, pkey_name, skey, skey_name
+    )
+    assert key_clause == expect
